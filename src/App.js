d0bd1f7e44bd27754315d61c321d2f3de55e033d
Naveen Kashyap <naveen@instabase.com>
2021-04-15 19:32:37 -0700

    changed text for feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Naveen!
+          Hi Naveen, this is from the feature branch!
         </p>
         <a
           className="App-link"
